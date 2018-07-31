4cd88e95398e11d473e8ec1cf7b51e49c8a79555
CaameronNakasone <caameron@pdx.edu>
2018-07-30 21:23:51 -0700

    Added in Flip feature by flipping the pixels on a image in the flip.rs file

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Pixel-Converter
 Pixel Converter for Rust Programming Language Class
+
+
+#Layout of Project
+There will be multiple files that will run the program. The main.rs file will take in an image from the command line and output a command-line menu
+where the user will be able to pick and choose what alterations they want to have done to the image. Each of the alterations will be implemented
+in different rust files and have functions that can be called from the main file. The output of the program will be another image with all of the
+alterations done to it.
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+extern crate image;
+
+mod flip;
+
+use image::GenericImage;
+use std::fs::File;
+
+//Main Function for the project.
 fn main() {
-    println!("Hello, world!");
+    
+    //Open image file
+    //let img = image::open("Test.jpg").expect("Image cannot be opened"); 
+    //println!("dimensions {:?}", img.dimensions());
+
+
+    let new_img = flip::flip_img("Test.jpg".to_string());
+
+    //Save new image as a output.png
+    let ref mut fout = File::create("output.png").expect("Cannot create file");
+    image::ImageRgba8(new_img).save("output.png").expect("unable to save file"); 
 }
+
