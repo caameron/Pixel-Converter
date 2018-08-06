734f99a306ccf93d03f3adcea923ba648f09265d
sunanth123 <33298889+sunanth123@users.noreply.github.com>
2018-08-06 13:40:10 -0700

    added comments

--- src/enlarge.rs
+++ src/enlarge.rs
@@ -1,3 +1,4 @@
+//This file contains the function to double the size of an image.
 extern crate image;
 use image::{GenericImage, ImageBuffer, Rgba};
 use imagestruct;
@@ -6,11 +7,12 @@ pub fn enlarge_img(img_struct: &imagestruct::MainImage) -> ImageBuffer<Rgba<u8>,
     let width = img_struct.width;
     let height = img_struct.height;
 
-    let mut enlarged = ImageBuffer::new(width * 2, height * 2);
+    let mut enlarged = ImageBuffer::new(width * 2, height * 2); //create new image with double dim.
 
     let mut e_width = 0;
     let mut e_height = 0;
 
+    //iterate through each pixel and place same pixel in 2x2 pixel block in new image to double size.
     for pixel in img_struct.img.pixels() {
         enlarged.put_pixel(e_width, e_height, pixel.2);
         enlarged.put_pixel(e_width + 1, e_height, pixel.2);
