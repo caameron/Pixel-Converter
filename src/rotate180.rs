6e02e97feedac0ff419658ce82b17781b8180231
sunanth123 <33298889+sunanth123@users.noreply.github.com>
2018-08-06 13:23:18 -0700

    added comments

--- src/rotate180.rs
+++ src/rotate180.rs
@@ -1,3 +1,4 @@
+//This file is used for the rotate image by 180 degrees.
 extern crate image;
 use image::{GenericImage, ImageBuffer, Rgba};
 use imagestruct;
@@ -6,11 +7,12 @@ pub fn rotate180_img(img_struct: &imagestruct::MainImage) -> ImageBuffer<Rgba<u8
     let width = img_struct.width;
     let height = img_struct.height;
 
-    let mut rotated = ImageBuffer::new(width, height);
+    let mut rotated = ImageBuffer::new(width, height);  //new image will have same dimensions as original
 
     let mut rot_width = width - 1;
     let mut rot_height = height - 1;
 
+    //iterate through each pixel in original image and put pixel in bottom up fashion for 180 rotate.
     for pixel in img_struct.img.pixels() {
         rotated.put_pixel(rot_width, rot_height, pixel.2);
         if rot_width == 0 {
